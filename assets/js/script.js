d6446b3a5ad59facb22cf583015ad28b7fbaccfd
Devon <Dwill629@gmail.com>
2021-01-10 22:14:01 -0500

    updated text and time to save to local storage in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 // varible to display todays current date //
 var currentDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentDate);
+
+$(document).ready(function () {
+    // click listener for the saveBtn 
+    $(".saveBtn").on("click", function () {
+        // to grab the values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // to save all text in your local storage input for the each time
+        localStorage.setItem(time, text);
+    })
+
+})
